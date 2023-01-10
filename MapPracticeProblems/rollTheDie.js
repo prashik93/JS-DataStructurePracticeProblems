0f28f973d1daf1a005561573b3aeb4128b5e7849
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-10 15:22:13 +0530

    MODIFIED : Q1.c - Store The Result Into Dictionary

--- MapPracticeProblems/rollTheDie.js
+++ MapPracticeProblems/rollTheDie.js
@@ -1,4 +1,8 @@
+let rollMap = new Map();
+
 for(let i = 0; i < 6; i++) {
     dieRoll = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-console.log(dieRoll);
+    console.log(dieRoll);
+    rollMap.set(i+1, dieRoll);
 }
+console.log(rollMap);
