3dc921047dab866fc2ecd013981783215763ace7
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-10 15:55:07 +0530

    MODIFIED : Q1.d - Repeat Till Any One Of The Number Has Reached 10 Times

--- MapPracticeProblems/rollTheDie.js
+++ MapPracticeProblems/rollTheDie.js
@@ -1,8 +1,20 @@
 let rollMap = new Map();
+let die = true;
+const dieReachCondition = 10;
+let count;
 
-for(let i = 0; i < 6; i++) {
+while(die) {
     dieRoll = Math.floor(Math.random() * (6 - 1 + 1) + 1);
-    console.log(dieRoll);
-    rollMap.set(i+1, dieRoll);
+    if(rollMap.has(dieRoll)) {
+        count = rollMap.get(dieRoll);
+        rollMap.set(dieRoll, count+1);
+    } else {
+        count = 1;
+        rollMap.set(dieRoll, count);
+    }
+    if(rollMap.get(dieRoll) == dieReachCondition){
+        die = false;
+    }
 }
+
 console.log(rollMap);
