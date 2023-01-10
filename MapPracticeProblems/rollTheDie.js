a0eed730b59fae32d1f095352c09172902cc4e3b
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-10 16:06:40 +0530

    MODIFIED : Q1.e - Find The Numbers That Occurs Max Time And Min Time In Map

--- MapPracticeProblems/rollTheDie.js
+++ MapPracticeProblems/rollTheDie.js
@@ -2,6 +2,10 @@ let rollMap = new Map();
 let die = true;
 const dieReachCondition = 10;
 let count;
+let maxValue = 0;
+let minValue = 10;
+let maxKey = 0;
+let minKey = 0;
 
 while(die) {
     dieRoll = Math.floor(Math.random() * (6 - 1 + 1) + 1);
@@ -17,4 +21,17 @@ while(die) {
     }
 }
 
+for(let i = 0; i <= rollMap.size; i++) {
+    if(rollMap.get(i) > maxValue) {
+        maxValue = rollMap.get(i);
+        maxKey = i;
+    }
+    if(rollMap.get(i) < minValue) {
+        minValue = rollMap.get(i);
+        minKey = i;
+    }
+}
+
 console.log(rollMap);
+console.log("The number that reached maximum number of times is : " + maxKey);
+console.log("The number that reached minimum number of times is : " + minKey);
